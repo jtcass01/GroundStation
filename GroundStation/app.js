195124b98fb624308ef057727aa890900563968b
Jacob Taylor Cassady <jtcass01@louisville.edu>
2017-08-25 21:09:47 -0400

    Added RCR images and additional css styling.

--- GroundStation/app.js
+++ GroundStation/app.js
@@ -1,20 +1,22 @@
-'user strict';
+﻿'user strict';
 
 const express = require('express');
 const http = require('http');
-var path = require('path');
-
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-console.log(__dirname + 'views');
-
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-    response.render('index', { title: 'RCR Ground Station' });
+    response.render('welcome/index', { title: 'RCR Ground Station' });
+});
+
+app.get('/welcome', (request, response) => {
+    response.render('welcome/index', { title: 'RCR Ground Station' });
 });
 
 app.listen(port, (error) => {
