8c68ce895c9b718eb7eb6d1d7a9345454c3d6980
Jacob Taylor Cassady <jtcass01@louisville.edu>
2017-08-26 04:33:15 -0400

    updated css

--- GroundStation/app.js
+++ GroundStation/app.js
@@ -1,5 +1,6 @@
 ﻿'user strict';
 
+//npm-modules
 const express = require('express');
 const http = require('http');
 const path = require('path');
@@ -7,16 +8,22 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Create Routes
+const routes = require('./routes/index')
+
+// Setup view engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-    response.render('welcome/index', { title: 'RCR Ground Station' });
-});
+// Setup routes
+app.use('/', routes);
 
-app.get('/welcome', (request, response) => {
-    response.render('welcome/index', { title: 'RCR Ground Station' });
+// catch 404 and forward to error handler
+app.use((request, response, next) => {
+    var error = new Error('Not Found');
+    error.status = 404;
+    next(error);
 });
 
 app.listen(port, (error) => {
---
+++ GroundStation/routes/index.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (request, response) => {
+    response.render('welcome/index', { title: 'RCR Ground Station' });
+});
+
+router.get('/welcome', (request, response) => {
+    response.render('welcome/index', { title: 'RCR Ground Station' });
+});
+
+module.exports = router;
+
+
